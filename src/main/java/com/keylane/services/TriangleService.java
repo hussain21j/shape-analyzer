2f0992e73ff9ed07e5ea78076773a55c2ac819da
Tayab Hussain <hussain21j@gmail.com>
2019-06-04 21:48:59 +0200

    Update TriangleService.java

--- src/main/java/com/keylane/services/TriangleService.java
+++ src/main/java/com/keylane/services/TriangleService.java
@@ -1,10 +1,55 @@
-package com.keylane.services;
+package com.keylane.service;
 
 import com.keylane.dto.Shape;
+import com.keylane.dto.TriangleBySide;
 import com.keylane.entity.TriangleBySideEntity;
+import com.keylane.entity.TriangleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
 public class TriangleService implements ShapeService {
+    @Autowired
+    TriangleRepository triangleRepository;
+
+    /**
+     *saves a triangle in the data bse
+     * todo use the builder or the constructor
+     */
+    @Override
+    @Transactional
     public TriangleBySideEntity save(Shape shape) {
-        return null;
+        TriangleBySide triangle = (TriangleBySide) shape;
+        TriangleBySideEntity entity =  TriangleBySideEntity.builder().
+                firstSide(triangle.getFirstSide()).
+                secondSide(triangle.getSecondSide()).
+                thirdSide(triangle.getThirdSide()).build();
+
+        TriangleBySideEntity savedTriangle = triangleRepository.save(entity);
+        log.info("Triangle is saved in the DB with id :"+savedTriangle.getId());
+        return savedTriangle;
+    }
+
+    /**
+     * fetch all the triangles from the database and identify the unique triangles
+     */
+    @Override
+    public List<Shape> findUniqueShapes() {
+        List<TriangleBySideEntity> listTringleEntity = triangleRepository.findAll();
+        List<Shape> listTriangle = new ArrayList<>();
+        for (TriangleBySideEntity entity: listTringleEntity) {
+                Shape triangle = new TriangleBySide(entity.getFirstSide(), entity.getSecondSide(), entity.getThirdSide());
+                listTriangle.add(triangle);
+        }
+
+        listTriangle = listTriangle.stream().distinct().collect(Collectors.toList());
+        return listTriangle;
     }
 }
