5250ba87a101d0c0bc85d88ac1c27b7abf7753bf
Tayab Hussain <hussain21j@gmail.com>
2019-06-04 21:48:13 +0200

    Update ShapeService.java

--- src/main/java/com/keylane/services/ShapeService.java
+++ src/main/java/com/keylane/services/ShapeService.java
@@ -1,4 +1,10 @@
-package com.keylane.services;
+package com.keylane.service;
+
+import com.keylane.dto.Shape;
+
+import java.util.List;
 
 public interface ShapeService {
+    public Object save(Shape shape);
+    public List<Shape> findUniqueShapes();
 }
