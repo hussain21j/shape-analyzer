aa46f9d87b3db881ee8b205ac4a3e01ef20ed9cb
Tayab Hussain <hussain21j@gmail.com>
2019-06-04 21:44:56 +0200

    Update TriangleBySideEntity.java

--- src/main/java/com/keylane/entity/TriangleBySideEntity.java
+++ src/main/java/com/keylane/entity/TriangleBySideEntity.java
@@ -1,4 +1,30 @@
 package com.keylane.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+/**
+ * Todo: rename it to triangle by side
+ */
 public class TriangleBySideEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private double firstSide;
+    private double secondSide;
+    private double thirdSide;
+    private String type;
 }
