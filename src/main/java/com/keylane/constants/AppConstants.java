40670194db3a2ba3155773b531031f554e0fbf71
Tayab Hussain <hussain21j@gmail.com>
2019-06-04 21:43:17 +0200

    Update AppConstants.java

--- src/main/java/com/keylane/constants/AppConstants.java
+++ src/main/java/com/keylane/constants/AppConstants.java
@@ -1,5 +1,44 @@
 package com.keylane.constants;
 
-public class AppConstants {
+import com.keylane.dto.TriangleBySide;
 
+/**
+ * Constants for the application
+ */
+public class AppConstants {
+    /**
+     * Triangle type
+     *
+     * Todo: rename
+     */
+    public enum Geometry {
+        EQUILATERAL("Equilateral Triangle"),
+        ISOSCELES("Isosceles Triangle"),
+        SCALENE("Scalene triangle"),
+        RIGHT_ANGLE("Right Angle Triangle");
+
+        private String type;
+
+        Geometry(String type) {
+            this.type = type;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+
+    public enum Geometries {
+        TRIANGLE_BY_SIDES(TriangleBySide.class);
+
+        private Class type;
+
+        Geometries(Class type) {
+            this.type = type;
+        }
+
+        public Class getType() {
+            return type;
+        }
+    }
 }
