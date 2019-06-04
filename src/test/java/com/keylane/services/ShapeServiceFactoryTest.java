bbfef770d42d73b7949072c437d5bb42ccc35c55
Tayab Hussain <hussain21j@gmail.com>
2019-06-04 21:51:35 +0200

    Update ShapeServiceFactoryTest.java

--- src/test/java/com/keylane/services/ShapeServiceFactoryTest.java
+++ src/test/java/com/keylane/services/ShapeServiceFactoryTest.java
@@ -1,12 +1,34 @@
-package com.keylane.services;
+package com.keylane.service;
 
+import com.keylane.dto.TriangleBySide;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.ApplicationContext;
 
-import static org.junit.Assert.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class ShapeServiceFactoryTest {
 
+    @Mock
+    ApplicationContext context;
+
+    @InjectMocks
+    ShapeServiceFactory serviceFactory;
+
     @Test
-    public void getShapeService() {
+    //todo: write assertion to check if shape service is of required type
+    public void getShapeService() throws Exception {
+        //Given
+        TriangleService triangleService = new TriangleService();
+        Mockito.when(context.getBean(TriangleService.class)).thenReturn(triangleService);
+        //When
+        ShapeService shapeService =  serviceFactory.getShapeService(TriangleBySide.class);
+        //Then
+        Assertions.assertThat(shapeService).isNotNull();
     }
+
 }
