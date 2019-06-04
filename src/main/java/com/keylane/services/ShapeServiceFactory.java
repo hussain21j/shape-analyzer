4a1aabf1308ec5c70be3d8ead2abcfb2e37b916e
Tayab Hussain <hussain21j@gmail.com>
2019-06-04 21:48:33 +0200

    Update ShapeServiceFactory.java

--- src/main/java/com/keylane/services/ShapeServiceFactory.java
+++ src/main/java/com/keylane/services/ShapeServiceFactory.java
@@ -1,9 +1,33 @@
-package com.keylane.services;
+package com.keylane.service;
 
-import com.keylane.exceptions.NoServiceAvailableException;
+import com.keylane.dto.TriangleBySide;
+import com.keylane.exception.NoServiceAvailable;
+import org.springframework.context.ApplicationContext;
 
+/**
+ * Factory to get the service for a specific type of shape
+ */
 public class ShapeServiceFactory {
-    public ShapeService getShapeService(Class Shape) throws NoServiceAvailableException {
-        return null;
+    ApplicationContext context;
+    TriangleService triangleService;
+
+    /**
+     * constructor of the class, initializes the bean of all the all concrete shape services
+     */
+    ShapeServiceFactory( ApplicationContext applicationContext) {
+        this.context = applicationContext;
+    }
+
+    /**
+     * Finds and returns the service for a shape
+     */
+    ShapeService getShapeService(Class shape) throws NoServiceAvailable{
+        ShapeService shapeService;
+        if(shape == TriangleBySide.class)
+            shapeService = context.getBean(TriangleService.class);
+        else {
+            throw new NoServiceAvailable("No service available for shape "+shape.getClass());
+        }
+        return shapeService;
     }
 }
