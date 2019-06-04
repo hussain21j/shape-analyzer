587c81b676f3bd2e12e34c5ffba694f80989dc68
Tayab Hussain <hussain21j@gmail.com>
2019-06-04 21:51:10 +0200

    Update ShapeAnalyzerTest.java

--- src/test/java/com/keylane/services/ShapeAnalyzerTest.java
+++ src/test/java/com/keylane/services/ShapeAnalyzerTest.java
@@ -1,12 +1,62 @@
-package com.keylane.services;
+package com.keylane.service;
 
+import com.keylane.constants.AppConstants;
+import com.keylane.dto.Shape;
+import com.keylane.dto.TriangleBySide;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ShapeAnalyzerTest {
+    @Mock
+    ShapeServiceFactory serviceFactory;
+
+    @Mock
+    ShapeService service;
+
+    @InjectMocks
+    ShapeAnalyzer analyzer;
 
     @Test
-    public void analyze() {
+    public void analyze() throws Exception {
+        //Given
+        ShapeAnalyzer spyShapeAnalyzer  = Mockito.spy(analyzer);
+        Mockito.doReturn(serviceFactory).when(spyShapeAnalyzer).getServiceFactory();
+        Shape shape = new TriangleBySide(100.00, 150.00, 200.00);
+        Mockito.when(serviceFactory.getShapeService(TriangleBySide.class)).thenReturn(service);
+        Mockito.when(service.save(ArgumentMatchers.any())).thenReturn(null);
+
+        //When
+        String shapeType = spyShapeAnalyzer.analyze(shape);
+
+        //Then
+        Assertions.assertThat(shapeType).isEqualToIgnoringCase(AppConstants.Geometry.SCALENE.getType());
     }
+
+    @Test
+    public void getUniqueShapes() throws Exception {
+        //Given
+        //Given
+        ShapeAnalyzer spyShapeAnalyzer  = Mockito.spy(analyzer);
+        Mockito.doReturn(serviceFactory).when(spyShapeAnalyzer).getServiceFactory();
+        List<Shape> listShape = new ArrayList<>();
+        listShape.add(new TriangleBySide(100.00, 150.00, 200.00));
+        Mockito.when(serviceFactory.getShapeService(TriangleBySide.class)).thenReturn(service);
+        Mockito.when(service.findUniqueShapes()).thenReturn(listShape);
+        //When
+        List<Shape> receivedShapeList =  spyShapeAnalyzer.getUniqueShapes(AppConstants.Geometries.TRIANGLE_BY_SIDES);
+        //Then
+        Assertions.assertThat(receivedShapeList).hasSize(1);
+        Assertions.assertThat( ((TriangleBySide)receivedShapeList.get(0)).getFirstSide()).isEqualTo(100.00);
+    }
+
 }
